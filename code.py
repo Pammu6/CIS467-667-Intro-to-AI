eba80934a61795bb79c125c629151e951debae03
KARTHEEK <48691379+Pammu6@users.noreply.github.com>
2020-02-20 15:52:37 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,22 @@
+"""
+A little code question to understand the formatting and scoring.
+Run tests.py to see your score (it will be the last line of output).
+Your homework grade may also be based on private tests you can not see.
+"""
+
+netid = "gkatz01" # replace with your NetID!
+
+def hello():
+    """
+    Return a string of the form "Hello <your netid>".
+    For example, mine would return "Hello gkatz01".
+    """
+    raise(NotImplementedError) # remove this line when you are done
+
+if __name__ == "__main__":
+    """
+    You can edit code in this if statement for informal testing.
+    When you run this file as a python script, it will execute this code.
+    This code will not impact the score on the automated test.
+    """
+    print(hello())
